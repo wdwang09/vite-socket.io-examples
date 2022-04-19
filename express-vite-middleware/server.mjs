cbf82087b4231c26908796c651de8b36e57f1de5
wdwang <wdwang09+git@outlook.com>
2022-04-19 20:42:43 +0800

    express vite middleware

---
+++ express-vite-middleware/server.mjs
@@ -0,0 +1,43 @@
+import { env } from 'process'
+import { createServer as createHttpServer } from 'http'
+import express from 'express'
+import { createServer as createViteServer } from 'vite'
+import { Server } from 'socket.io'
+;(async () => {
+  const productionRootPath = 'dist'
+  const isProductionMode = env.NODE_ENV === 'production'
+
+  const app = express()
+  if (!isProductionMode) {
+    console.log('> Development Mode')
+    const vite = await createViteServer({
+      server: { middlewareMode: 'html' }
+    })
+    app.use(vite.middlewares)
+  } else {
+    console.log('> Production Mode')
+    app.use(express.static(productionRootPath))
+  }
+
+  const httpServer = createHttpServer(app)
+  const io = new Server(httpServer)
+
+  io.on('connection', socket => {
+    console.log(`connect: ${socket.id}`)
+
+    socket.on('hello!', () => {
+      console.log(`hello from ${socket.id}`)
+    })
+
+    socket.on('disconnect', () => {
+      console.log(`disconnect: ${socket.id}`)
+    })
+  })
+
+  setInterval(() => {
+    io.emit('message', new Date().toISOString())
+  }, 1000)
+
+  httpServer.listen(3000)
+  console.log('> Local: http://localhost:3000/')
+})()
---
+++ express-vite-middleware/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import { io } from 'socket.io-client'
+
+defineProps({
+  msg: String
+})
+
+const socket = io()
+
+const isConnected = ref(false)
+const lastMessage = ref(null)
+
+onMounted(() => {
+  socket.on('connect', () => {
+    isConnected.value = true
+  })
+  socket.on('disconnect', () => {
+    isConnected.value = false
+  })
+  socket.on('message', data => {
+    lastMessage.value = data
+  })
+})
+
+onUnmounted(() => {
+  socket.disconnect()
+})
+
+function sendMessage() {
+  socket.emit('hello!')
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p>Connected: {{ isConnected }}</p>
+  <p>Last message: {{ lastMessage || '-' }}</p>
+  <button type="button" @click="sendMessage">Say hello!</button>
+</template>
